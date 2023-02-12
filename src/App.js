82f967cbf0b50fa32408c8f1fae33e39514be624
Jordan <jordanelley@hotmail.com>
2023-02-12 21:53:20 +1300

    n/a

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import rock from './rock.png'
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
+
+    const [age, setAge] = useState(0)
+    const [isClicked, setClicked] = useState(false)
+    const clicked = (event) => {
+        setClicked(true);
+
+    }
+
+    const handleChange = (event) => {
+        const age = event.target.value;
+        setAge(Number(age));
+        setClicked(false);
+    };
+
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <button> Login </button> <button> Sign Up</button>
+        <img src={rock} className="App-logo" alt="logo" />
+        <h1>
+            Are you older that Dwayne Johnson
+        </h1>
+          <div>
+              <label>
+                  your age:
+                  <input type="text" onChange={handleChange} />
+              </label>
+              <input type="submit" value="Submit" onClick={clicked}/>
+              <div className="result">
+                  <h1>
+              {isClicked? (age<50? "no": "yes"):""}</h1></div>
+          </div>
       </header>
     </div>
   );
